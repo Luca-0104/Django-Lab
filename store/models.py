81ac7f4c6ca0a667e3c062c9160c71bc1ef44d1d
LiuZhe <lzhe72537@gmail.com>
2023-11-12 01:37:32 -0500

    one to many

--- store/models.py
+++ store/models.py
@@ -1,20 +1,12 @@
 from django.db import models
 
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)    
 
-class Order(models.Model):
-    PAYMENT_STATUS_PENDING = 'P'
-    PAYMENT_STATUS_COMPLETE = 'C'
-    PAYMENT_STATUS_FAILED = 'F'
 
-    PAYMENT_STATUS_CHOICES = [
-        (PAYMENT_STATUS_PENDING, "Pending"),
-        (PAYMENT_STATUS_COMPLETE, "Complete"),
-        (PAYMENT_STATUS_FAILED, "Failed")
-    ]
-
-    place_at = models.DateTimeField(auto_now=True)
-    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+class Cart(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
 
 
 class Customer(models.Model):
@@ -36,16 +28,54 @@ class Customer(models.Model):
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, "Pending"),
+        (PAYMENT_STATUS_COMPLETE, "Complete"),
+        (PAYMENT_STATUS_FAILED, "Failed")
+    ]
+
+    place_at = models.DateTimeField(auto_now=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    # one to many (1 customer -> n orders)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
-    last_update = models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)    
+    # one to many (1 collection -> n product)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+
+class CartItem(models.Model):
+    # many to many (1 Cart -> n CartItem)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    # many to many (1 Product -> n CartItem)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+
+
+class OrderItem(models.Model):
+    # one to many (1 Order -> n OrderItem)
+    Order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    # one to many (1 Product -> n OrderItem)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     # one to one relation with Customer
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    # customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    # one to many (One customer -> many Address)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
